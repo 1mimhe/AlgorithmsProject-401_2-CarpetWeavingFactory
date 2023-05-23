95d96be1f4cc18a473317ef5dc6b680e0284d60c
MimHe <mimhe1381@gmail.com>
2023-05-23 23:16:57 +0330

    Add 'carpets' List

--- Main.java
+++ Main.java
@@ -1,4 +1,9 @@
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
+    public static ArrayList<Carpet> carpets = new ArrayList<>();
     public static void main(String[] args) {
 
     }
