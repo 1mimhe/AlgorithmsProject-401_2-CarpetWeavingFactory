7e250b1c0031770f777cf7d2e036016c5f977d78
Behnood <behnoodoboodiyyat@gmail.com>
2023-05-24 11:16:46 +0330

    add 'buyCarpet' method

--- Graph.java
+++ Graph.java
@@ -1,17 +1,19 @@
 import java.util.Arrays;
 
+import static java.lang.Math.max;
+
 public class Graph {
+
     public static void designNewCarpet(Carpet newCarpet, int n) {
         int[] result = new int[n];
 
-        result[0]  = 0;
+        result[0] = 0;
 
         boolean[] availableForPaint = new boolean[n];
         Arrays.fill(availableForPaint, true);
 
         // Assign colors to remaining n-1 vertices
-        for (int i = 1; i < n; i++)
-        {
+        for (int i = 1; i < n; i++) {
             for (var list : newCarpet.getGraph()[i]) {
                 if (result[list.value] != -1)
                     availableForPaint[result[list.value]] = false;
@@ -19,7 +21,7 @@ public class Graph {
 
             // Find the first availableForPaint color
             int cr;
-            for (cr = 0; cr < n; cr++){
+            for (cr = 0; cr < n; cr++) {
                 if (availableForPaint[cr])
                     break;
             }
@@ -40,4 +42,21 @@ public class Graph {
                     + result[i]);
         Main.carpets.add(newCarpet);
     }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    static int buyCarpet(int W, int[] weight, int[] value , int n) {
+
+
+        if (value.length == 0 || W == 0)
+            return 0;
+
+
+        if (weight[value.length - 1] > W)
+            return buyCarpet(W, weight, value, value.length - 1);
+
+
+        else return max(value[value.length - 1] + buyCarpet(W - weight[value.length - 1], weight, value, value.length - 1),
+                buyCarpet(W, weight, value, value.length - 1)
+        );
+    }
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
