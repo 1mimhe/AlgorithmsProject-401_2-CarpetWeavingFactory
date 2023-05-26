ebfaf26c8cc57eab4fb453bed9b23d2227661a43
MimHe <mimhe1381@gmail.com>
2023-05-26 12:37:05 +0330

    Add 'designPanel' method

--- Panel.java
+++ Panel.java
@@ -1,2 +1,27 @@
 public class Panel {
+    public static void designPanel() {
+        System.out.println("*Design Panel*");
+
+        System.out.print("Enter the Price of Your Carpet=> ");
+        double price = Main.input.nextDouble();
+
+        System.out.print("Number of Carpet's Patterns=>");
+        int numOfPatterns = Main.input.nextInt();
+        Main.input.nextLine();
+
+        System.out.println("Enter Relations Between Patterns.");
+        System.out.println("[Patterns=>0...n-1] [Relations=> Ex: 0 5, And 0 For Exit]");
+
+        Carpet newCarpet = new Carpet(numOfPatterns, price);
+
+        int i = 1;
+        while (true){
+            System.out.print("Relation " + i + "=> ");
+            String[] k = Main.input.nextLine().split(" ");
+            if (k.length == 1) break;
+            newCarpet.addEdge(Integer.parseInt(k[0]), Integer.parseInt(k[1]));
+        }
+
+        Controller.designNewCarpet(newCarpet, numOfPatterns);
+    }
 }
